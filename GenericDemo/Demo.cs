937998963c229d92e647f5a52ecdbe59b15aff47
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-20 20:25:26 +0530

    [sanjana] Add : Integer Max of three

---
+++ GenericDemo/Demo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class Demo
+    {
+        public void MaxInteger(int a,int b,int c)
+        {
+            if(a.CompareTo(b) >0 && a.CompareTo(c)>0)
+            {
+                Console.WriteLine(a + "is Max");
+            }
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+            {
+                Console.WriteLine(b + "is Max");
+            }
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
+            {
+                Console.WriteLine(c + "is Max");
+            }
+
+
+        }
+    }
+}
--- GenericDemo/Program.cs
+++ GenericDemo/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class Program
+    {
+      static void Main(string[] args)
+        {
+            Demo demo = new Demo();
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("Select Option\n1.MaxInteger\n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        demo.MaxInteger(10, 50, 100);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
